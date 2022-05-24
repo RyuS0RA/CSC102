8ece2225365afa2c386cc0f35745f8f5df3a6b05
RyuS0RA <84358052+RyuS0RA@users.noreply.github.com>
2022-05-24 19:19:38 -0400

    Update BlastOff.js

--- BlastOff.js
+++ BlastOff.js
@@ -1,13 +1,14 @@
 function StartTheCountdown()
 {
+    //This determines where the countdown Begins
     var countdown = 10;
 
-
+    //This is the countdown 
     for (var i = 1; i <= 11; i++)
     {
         
 
-
+        //This is the part that determins how the timer ends and what is displayed
         if (i == 11)
         {
             setTimeout(function() {
@@ -15,7 +16,7 @@ function StartTheCountdown()
             }, 1000 * i)
         
         }
- 
+        //This tells the timer to keep going
         else {
             setTimeout(function () {
                 document.getElementById("CountdownDisplay").innerHTML = countdown + " And Counting!";
